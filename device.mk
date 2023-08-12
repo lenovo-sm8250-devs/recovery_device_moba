0d3445bed9a8dd3e894b539c50d8e6d4a53ff7b9
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-08-12 15:17:53 -0300

    moba: add libion.so

--- device.mk
+++ device.mk
@@ -11,6 +11,13 @@ LOCAL_PATH := device/lenovo/moba
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
 
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+
+# qcom decrypt
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
