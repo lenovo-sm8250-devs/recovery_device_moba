5166922b52774267ba66ab99e578cc2a3ba348e2
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-08-12 13:12:08 -0300

    moba: remove libandroidicu due to build error

--- device.mk
+++ device.mk
@@ -11,6 +11,3 @@ PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
-# Apex libraries
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
