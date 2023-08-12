4b4ee56539ad29134e5ae053387d68a650001df2
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-08-12 12:35:17 -0300

    moba: add libandroidicu.so libraries

--- device.mk
+++ device.mk
@@ -10,3 +10,7 @@ LOCAL_PATH := device/lenovo/moba
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
