7e020badbdfa53c3140c40c10af9d90fe34afe2c
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-08-12 16:51:18 -0300

    moba: add product_release_name to fix wrong path error's

--- twrp_moba.mk
+++ twrp_moba.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := moba
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
