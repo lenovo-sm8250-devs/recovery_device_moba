e875cb2e7ac4b412146c370e18c636fc25f216e2
isaquetp <superboydbr@gmail.com>
2023-08-16 03:47:35 +0000

    moba: system_root not mounting fully fixed
    Signed-off-by: isaquetp <superboydbr@gmail.com>

--- device.mk
+++ device.mk
@@ -11,9 +11,17 @@ LOCAL_PATH := device/lenovo/moba
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Recovery Modules
 PRODUCT_HOST_PACKAGES += \
     libandroidicu
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
 
 # qcom decrypt
 PRODUCT_PACKAGES += \
