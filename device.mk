b43e3d610949110b3bc9d856d9219b83ca9eb6db
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-08-16 22:16:42 -0300

    moba: add moba_keymaster_version flag

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_PACKAGES += \
     qcom_decrypt_fbe
 
 # Keymaster
+MOBA_KEYMASTER_VERSION := 3
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
