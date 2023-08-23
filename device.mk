db3573af388fe274e40f13703b701d875f1422ab
dreAd <68554241+dreAd1811@users.noreply.github.com>
2023-08-23 13:31:21 +0530

    moba: fix MOBA_KEYMASTER_VERSION

--- device.mk
+++ device.mk
@@ -29,7 +29,7 @@ PRODUCT_PACKAGES += \
     qcom_decrypt_fbe
 
 # Keymaster
-MOBA_KEYMASTER_VERSION := 3
+MOBA_KEYMASTER_VERSION := 3.0
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
